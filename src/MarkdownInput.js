ca12423fd16a08c9f6175898d47bbf72b796bd6d
Vestimir Markov <me@vestimir.com>
2021-06-12 08:18:30 +0300

    Simplify preview

--- src/MarkdownInput.js
+++ src/MarkdownInput.js
@@ -14,45 +14,6 @@ const ICONS = {
     "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iOCIgaGVpZ2h0PSIxMiIgdmlld0JveD0iMCAwIDggMTIiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGQ9Ik0xIDMuMDA0NDhDMS4yMzUxIDIuMzM2MTQgMS42OTkxNSAxLjc3MjU4IDIuMzA5OTUgMS40MTM2MUMyLjkyMDc2IDEuMDU0NjMgMy42Mzg5IDAuOTIzNDE0IDQuMzM3MTggMS4wNDMxOUM1LjAzNTQ2IDEuMTYyOTYgNS42Njg4MiAxLjUyNiA2LjEyNTA4IDIuMDY4QzYuNTgxMzQgMi42MTAwMSA2LjgzMTA2IDMuMjk2IDYuODMgNC4wMDQ0OEM2LjgzIDYuMDA0NDggMy44MyA3LjAwNDQ4IDMuODMgNy4wMDQ0OCIgc3Ryb2tlPSJ3aGl0ZSIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIvPgo8cGF0aCBkPSJNMy45MTAwMyAxMS4wMDQ1SDMuOTIwMDMiIHN0cm9rZT0id2hpdGUiIHN0cm9rZS13aWR0aD0iMS41IiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPC9zdmc+Cg==",
 };
 
-const ModalWrapper = styled.div`
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 1000;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.6);
-`;
-
-const CloseButton = styled.button`
-  ${buttonReset};
-  position: absolute;
-  top: 0;
-  right: 0;
-  z-index: 1001;
-  width: 32px;
-  height: 32px;
-  background: transparent;
-  color: ${(props) => props.theme.colors.black};
-  font-size: 32px;
-  line-height: 32px;
-`;
-
-const ModalContent = styled.div`
-  position: relative;
-  width: 90%;
-  background-color: ${(props) => props.theme.colors.white};
-`;
-
-const ModalScroll = styled.div`
-  height: 80vh;
-  padding: 2em;
-  overflow-y: auto;
-`;
-
 const IconButton = styled.button`
   ${buttonReset};
   width: 2em;
@@ -88,23 +49,12 @@ const ButtonToolbar = styled.div`
   gap: 0.5em;
 `;
 
-const Modal = ({ children, onClose }) => (
-  <ModalWrapper>
-    <ModalContent>
-      <CloseButton type="button" onClick={onClose}>
-        &times;
-      </CloseButton>
-
-      <ModalScroll>{children}</ModalScroll>
-    </ModalContent>
-  </ModalWrapper>
-);
-
-const PreviewModal = ({ content, onClose }) => (
-  <Modal onClose={onClose}>
-    <div dangerouslySetInnerHTML={{ __html: toHTML(content) }} />
-  </Modal>
-);
+const PreviewPane = styled.div`
+  box-sizing: border-box;
+  padding: 1em;
+  overflow-y: auto;
+  border: 1px dashed ${(props) => props.theme.colors.primary};
+`;
 
 const MarkdownInput = ({
   label = "Text",
@@ -121,21 +71,33 @@ const MarkdownInput = ({
       {label && <Label>{label}</Label>}
 
       <TextareaWrapper>
-        <Textarea
-          value={value}
-          style={{
-            height,
-            resize: "vertical",
-          }}
-          onChange={(e) => onChange(e.target.value, e)}
-          {...props}
-        />
+        {!open && (
+          <Textarea
+            value={value}
+            style={{
+              height,
+              resize: "vertical",
+            }}
+            onChange={(e) => onChange(e.target.value, e)}
+            {...props}
+          />
+        )}
+
+        {open && (
+          <PreviewPane
+            style={{
+              height,
+            }}
+          >
+            <div dangerouslySetInnerHTML={{ __html: toHTML(value) }} />
+          </PreviewPane>
+        )}
 
         <ButtonToolbar>
           <IconButton
             type="button"
             style={{ backgroundImage: `url(${ICONS.eye})` }}
-            onClick={() => setOpen(true)}
+            onClick={() => setOpen(!open)}
           >
             Preview
           </IconButton>
@@ -152,8 +114,6 @@ const MarkdownInput = ({
           </IconButton>
         </ButtonToolbar>
       </TextareaWrapper>
-
-      {open && <PreviewModal content={value} onClose={() => setOpen(false)} />}
     </FormGroup>
   );
 };
