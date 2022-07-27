f403a9afac9229ed97dc37616e311c62dcd9a04c
Vestimir Markov <me@vestimir.com>
2022-07-27 11:16:15 +0300

    Add attributes to markdown parser

--- src/toHTML.js
+++ src/toHTML.js
@@ -1,8 +1,14 @@
 import MarkdownIt from "markdown-it";
+import markdownItAttrs from "markdown-it-attrs";
 
 const parser = new MarkdownIt({
   html: true,
   breaks: true,
+}).use(markdownItAttrs, {
+  // optional, these are default options
+  leftDelimiter: "{",
+  rightDelimiter: "}",
+  allowedAttributes: [], // empty array = all attributes are allowed
 });
 
 const toHTML = (string, inline = false) => {
